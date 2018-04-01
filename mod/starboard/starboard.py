1ac0899b81e9a6a72a908c065d8cffa54fd06300
Alexander G. Shuping <alexandershuping@gmail.com>
2018-04-01 02:07:11 -0400

    Starboard module is working.

--- mod/starboard/starboard.py
+++ mod/starboard/starboard.py
@@ -9,7 +9,7 @@ from util.AdminFramework import AdminFramework
 from util.Logger import Logger
 from util.StarFramework import Starboard, StarMessage
 
-class Starboard:
+class Starboard_cog:
 	''' Allows users to "star" significant messages by command or by emoji
 	    reaction. With enough stars, the message is copied onto a
 			"starboard" channel.
@@ -18,15 +18,36 @@ class Starboard:
 		self.bot = bot
 		self.flags = FlagFramework()
 		self.log = Logger()
+
+
+
+	async def on_reaction_add(self, reaction, user):
+		''' Check for actual star emoji adding '''
+		if user == self.bot.user:
+			return # Prevent recursion
+		ACCEPTABLE_REACTIONS = ['⭐','🌠','🌟','🔯','✡','✴','❇',]
+		if reaction.emoji in ACCEPTABLE_REACTIONS:
+			sb = Starboard(reaction.message.server, self.bot)
+			sm = sb.getStarboardMessage(reaction.message.channel.id, reaction.message.id)
+			sm.star(reaction.message.author.id)
+			await sm.updateStarboard(reaction.message.server, self.bot)
+			
+
 	
 
 
-	@commands.group(invoke_without_command=True,pass_context=True)
-	async def star(self, ctx):
+	@commands.command(pass_context=True)
+	async def startest(self, ctx):
+		await self.bot.say('test')
+	
+
+
+	@commands.group(pass_context=True, invoke_without_command=True)
+	async def stars(self, ctx):
 		''' Starboard-related commands ''' 
 		p = Phrasebook(ctx, self.bot)
-		if self.flags.hasFlag('channel', 'star', ctx.message.server):
-			cha = self.bot.get_channel(self.flags.getFlag('channel', 'star', ctx.message.server))
+		if self.flags.hasFlag('channel', 'star', ctx.message.server.id):
+			cha = self.bot.get_channel(self.flags.getFlag('channel', 'star', ctx.message.server.id)[0]['flag'])
 			if cha:
 				await self.bot.say(p.pickPhrase('starboard', 'working', cha.mention))
 		else:
@@ -34,7 +55,7 @@ class Starboard:
 	
 
 
-	@star.command(pass_context=True)
+	@stars.command(pass_context=True)
 	async def setup(self, ctx, threshold=None, channel=None):
 		''' Sets up the starboard. Provide a channel id to start it in that
 		    channel; otherwise, it will start in whatever channel you send 
@@ -68,18 +89,20 @@ class Starboard:
 			else:
 				await self.bot.say(p.pickPhrase('starboard', 'setup', cha.mention))
 				sthresh = random.randrange(1,20)
-				if self.flags.hasFlag('thresh', 'star', ctx.message.server):
-					sthresh = self.flags.getFlag('thresh', 'star', ctx.message.server)
+				if self.flags.hasFlag('thresh', 'star', ctx.message.server.id):
+					sthresh = self.flags.getFlag('thresh', 'star', ctx.message.server.id)[0]['flag']
 				else:
-					await self.bot.say(p.pickPhrase('starboard', 'needsthresh'))
+					await self.bot.say(p.pickPhrase('starboard', 'needsthresh') + '\n\n**(waiting for response)**')
 					msg = None
 					while True:
-						msg = self.bot.wait_for_message(timeout=10)
+						msg = await self.bot.wait_for_message(timeout=10)
 						if not msg:
 							await self.bot.say(p.pickPhrase('starboard', 'nothresh', sthresh))
 							break
+						elif msg.author == self.bot.user:
+							continue
 						elif not a.check(msg.author):
-							await self.bot.say(p.pickPhrase('starboard', 'badconfirmer', msg.author.name))
+							await self.bot.say(p.pickPhrase('starboard', 'badconfirmer', msg.author.name) + '\n\n**(waiting for _administrator_ response)**')
 						else:
 							try:
 								if int(msg.content) > 0:
@@ -87,52 +110,69 @@ class Starboard:
 									await self.bot.say(p.pickPhrase('starboard', 'working', cha.mention))
 									break
 								else:
-									await self.bot.say(p.pickPhrase('starboard', 'negative'))
+									await self.bot.say(p.pickPhrase('starboard', 'negative') + '\n\n**(waiting for ``P O S I T I V E`` response)**')
 							except Exception:
-								await self.bot.say(p.pickPhrase('starboard', 'badthresh', thresh))
+								await self.bot.say(p.pickPhrase('starboard', 'badthresh', sthresh) + '\n\n**(waiting for _valid_ response)**')
 
-				Starboard s(ctx.message.server)
+				s = Starboard(ctx.message.server, self.bot)
 				s.reset(cha, sthresh)
 
 					
 	
-	@star.command(pass_context=True)
+	@stars.command(pass_context=True)
 	async def threshold(self, ctx, thresh):
+		''' [Administrators only]
+				Set the starboard threshold to the value specified in <thresh>.
+				Note that the value must be greater than zero.
+		'''
 		p = Phrasebook(ctx, self.bot)
 		a = AdminFramework(ctx.message.server)
 		if not a.check(ctx.message.author):
 			await self.bot.say("This command is for administrators only!")
 			return
 		else:
-			Starboard s(ctx.message.server)
+			s = Starboard(ctx.message.server, self.bot)
 			if not s.isWorking():
-				await self.bot.say(p.pickPhrase('starboard','offline')
+				await self.bot.say(p.pickPhrase('starboard','offline'))
 			else:
-				if thresh <= 0:
-					await self.bot.say("The star threshold has to be greater than zero!")
+				if int(thresh) <= 0:
+					await self.bot.say(p.pickPhrase('starboard', 'negative'))
 				else:
-					s.reset(s.channel, thresh)
+					s.reset(s.channel, int(thresh))
+					await self.bot.say('Confirmed.')
 	
 
 
-	@star.command(pass_context=True)
-	async def star(self, ctx, messid, chanid=None):
+	@stars.command(pass_context=True)
+	async def star(self, ctx, messid, chanid=None, servid=None):
+		''' Add a star to the message with the given ID. Provide a channel
+				or server id if you want to star a message from a different 
+				channel/server
+		'''
 		p = Phrasebook(ctx, self.bot)
+		srv = ctx.message.server
 		cha = ctx.message.channel
+		mes = None
+		if servid:
+			userSrv = self.bot.get_server(servid)
+			if usrSrv:
+				srv = usrSrv
 		if chanid:
-			cha = chanid
+			usrCha = srv.get_channel(chanid)
+			if usrCha:
+				cha = usrCha
 
-		found = self.bot.get_message(cha, str(messid))
-		if not found:
+		mes = await self.bot.get_message(cha, messid)
+		if not mes:
 			await self.bot.say(p.pickPhrase('starboard', 'messagenotfound'))
 			return
 
-		sm = StarMessage(ctx.message.server, ctx.message.channel, messid)
-		sb = Starboard(ctx.message.server)
+		sb = Starboard(ctx.message.server, self.bot)
+		sm = sb.getStarboardMessage(mes.channel.id, mes.id)
 		sm.star(ctx.message.author.id)
-		if #TODO left off here
+		await sm.updateStarboard(ctx.message.server, mes.channel, self.bot)
 	
 
 
 def setup(bot):
-	bot.add_cog(Admin(bot))
+	bot.add_cog(Starboard_cog(bot))
--- util/StarFramework.py
+++ util/StarFramework.py
@@ -17,36 +17,119 @@ class StarThresholdError:
 
 class Starboard:
 	''' Class representing a server's Starboard '''
-	def __init__(self, server):
+	def __init__(self, server, bot):
+		self.log = Logger()
+		self.base = CabbageBase()
 		self.flags = FlagFramework()
 		self.server = server
-		self.channel = self.flags.getFlag('channel', 'star', self.server)
-		self.thresh = self.flags.getFlag('thresh', 'star', self.server)
+		self.bot = bot
+		self.sbChanid = None
+		self.sbChannel = None
+		self.thresh = None
+		if self.flags.hasFlag('channel', 'star', self.server.id):
+			self.sbChanid = self.flags.getFlag('channel', 'star', self.server.id)[0]['flag']
+			self.sbChannel = self.server.get_channel(self.sbChanid)
+		if self.flags.hasFlag('thresh', 'star', self.server.id):
+			self.thresh = self.flags.getFlag('thresh', 'star', self.server.id)[0]['flag']
 
 
 
 	def isWorking(self):
-		return self.channel and self.thresh and self.thresh > 0
+		''' Returns true if the server's starboard has been set up and is
+				valid.
+		'''
+		return self.sbChannel and self.thresh and self.thresh > 0
 
 
 
-	def reset(self, channel, thresh=None):
-		self.channel = channel
+	def reset(self, channel, thresh):
+		''' Resets the starboard parameters to the provided ones. 
+		'''
+		self.sbChannel = channel
 		if thresh > 0:
 			self.thresh = thresh
 		else:
 			raise StarThresholdError
 		self.log.log('Starboard for server ' + self.server.id + ' updated: parameters changed', 'star', 7)
-		self.update()
+		self._update()
 
 
 
-	def update(self):
+	def _update(self):
+		''' Updates the server flags and database entries for the starboard.
+				Note that this does NOT check any messages for new stars or
+				update starboard messages.
+		'''
 		if self.thresh <= 0:
 			raise StarThresholdError
-		self.flags.tset('channel', 'star', self.server, self.channel)
-		self.flags.iset('thresh', 'star', self.server, self.thresh)
+		self.flags.tset('channel', 'star', self.server.id, self.sbChannel.id)
+		self.flags.iset('thresh', 'star', self.server.id, self.thresh)
+		self.sbChanId = self.sbChannel.id
 
+
+
+	def getStarboardMessage(self, chanid, messid):
+		''' Return a StarMessage object for the provided message. 
+		'''
+		q = self.base.query('starboard', ('original_message_channel','starboard_message_messid'), (('server',int(self.server.id)),('original_message_channel',int(chanid)),('original_message_messid',int(messid))))
+		if q and len(q) > 0:
+			sm = StarMessage(self.server.id, q[0][0], messid, q[0][1])
+		else:
+			sm = StarMessage(self.server.id, chanid, messid)
+
+		return sm
+
+
+	
+	def _determineAppropriateStarEmoji(self, numStars):
+		''' Determines the appropriate star emoji
+		'''
+		if numStars < self.thresh:
+			return '⚫'
+		elif numStars < (1.5 * self.thresh):
+			return '⭐'
+		elif numStars < (2 * self.thresh):
+			return '✴'
+		elif numStars < (3 * self.thresh):
+			return '🌠'
+		else:
+			return '🌌'
+
+
+
+	async def _postOrUpdateStarboardMessage(self, msg, channel):
+		''' Posts a message to the starboard, or (if it is already there)
+				updates it to reflect changes in star totals.
+		'''
+		srv = self.bot.get_server(msg.server)
+		cha = channel
+		mes = await self.bot.get_message(cha, msg.messid)
+		sbMessid = None
+
+		if msg.starboardMessid:
+			sbMessid = msg.starboardMessid
+		else:
+			# The message indicates that it is not yet on the starboard, but
+			# check anyway.
+			q = self.base.query('starboard', ('starboard_message_messid',), (('server',int(self.server.id)),('original_message_channel',int(self.sbChanid)),('original_message_messid',int(msg.messid))))
+			if q and len(q) > 0:
+				# It was actually on the starboard.
+				sbMessid = q[0][0]
+
+		newEmbed = msg.constructEmbed(mes)
+		numStars = msg.getStars()
+		header = '**' + self._determineAppropriateStarEmoji(numStars) + str(numStars) + '   ' + cha.mention + '**'
+
+		if sbMessid:	
+			sbMessage = await self.bot.get_message(self.sbChannel, sbMessid)
+			await self.bot.edit_message(sbMessage, header, embed=newEmbed)
+		else:
+			newSbMes = await self.bot.send_message(self.sbChannel, header, embed=newEmbed)
+			cmd = 'INSERT INTO starboard (server, starboard, starboard_message_messid, original_message_channel, original_message_messid, original_message_sent) VALUES (%s,%s,%s,%s,%s,%s)'
+			cur = self.base.getCursor()
+			cur.execute(cmd, (int(self.server.id), int(self.sbChanid), int(newSbMes.id), int(mes.channel.id), int(mes.id), mes.timestamp))
+			self.base.commit()
+			cur.close()
 
 
 
@@ -54,17 +137,20 @@ class Starboard:
 
 class StarMessage:
 	''' Class representing a message with stars '''
-	def __init__(self, server, chanid, messid):
+	def __init__(self, server, chanid, messid, starboardMessid=None):
 		self.base = CabbageBase()
 		self.server = server
 		self.chanid = chanid
 		self.messid = messid 
+		self.starboardMessid = starboardMessid
 
 
 
 	def star(self, uid):
-	''' Adds a star to a message '''
-		if not self.hasStarred(self, uid):
+		''' Adds a star to a message, as long as it has not been starred by
+				the same user before.
+		'''
+		if not self.hasStarred(uid):
 			cur = self.base.getCursor()
 			insertString = 'INSERT INTO stars (server,chanid,messid,starrer) VALUES (%s,%s,%s,%s);'
 			cur.execute(insertString, (self.server, self.chanid, self.messid, uid))
@@ -74,6 +160,8 @@ class StarMessage:
 
 
 	def unstar(self, uid):
+		''' Removes a star from a message.
+		'''
 		if self.hasStarred(self, uid):
 			cur = self.base.getCursor()
 			delString = 'DELETE FROM ONLY stars WHERE server=%s AND chanid=%s AND messid=%s AND starrer=%s'
@@ -84,20 +172,45 @@ class StarMessage:
 
 
 	def getStars(self):
-		res = self.base.query('stars', ('server',), (('server', self.server), ('messid', self.messid)))
+		''' Returns the number of unique users who have starred the message.
+		'''
+		res = self.base.query('stars', ('server',), (('server', int(self.server)), ('messid', int(self.messid))))
 		return len(res)
 
 
 
 	def hasStarred(self, uid):
-		res = self.base.query('stars', ('server',), (('server', self.server), ('messid', self.messid), ('starrer', uid)))
+		''' Determines whether the provided user has previously starred the 
+				message.
+		'''
+		res = self.base.query('stars', ('server',), (('server', int(self.server)), ('messid', int(self.messid)), ('starrer', int(uid))))
 		return len(res) > 0
 
 
 
 	def constructEmbed(self, messageObject):
-		embed = discord.Embed(colour=discord.Colour(0x7f3e96), description=messageObject.text, timestamp=messageObject.timestamp)
+		''' Constructs the embed object to be used in the starboard message.
+		'''
+		embed = discord.Embed(colour=discord.Colour(0x7f3e96), description=messageObject.content, timestamp=messageObject.timestamp)
 
 		embed.set_author(name=messageObject.author.name, icon_url=messageObject.author.avatar_url)
 
+		if messageObject.attachments and len(messageObject.attachments) > 0:
+			attachment = messageObject.attachments[0]
+
+			if attachment['filename'].lower().endswith(('gif', 'jpg', 'jpeg', 'png')):
+				embed.set_image(url=attachment['url'])
+			else:
+				embed.add_field(name='Also attached',value=str(attachment['filename']))
+
 		return embed
+	
+
+
+	async def updateStarboard(self, serverObject, channelObject, bot):
+		''' Check if we're above the threshold value for the starboard. If so,
+				ask to be posted.
+		'''
+		s = Starboard(serverObject, bot)
+		if self.getStars() >= s.thresh:
+			await s._postOrUpdateStarboardMessage(self, channelObject)
